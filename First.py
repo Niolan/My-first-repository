2bb62b7e08e1600bbc11b826e90b816b51cec0f9
Niolan <noa-dis@yandex.ru>
2021-09-07 00:06:17 +0300

    The first task

--- First.py
+++ First.py
@@ -1,1 +1,17 @@
-print()
+def palin(text):
+    s = text.split()
+    for i in range(len(s)):
+        k = ''
+        for j in s[i]:
+            if j.isalpha():
+                k += j
+        s[i] = k[::-1]
+    s = ''.join(s)
+
+    for i in range(len(text)):
+        if not text[i].isalpha():
+            s = s[:i] + text[i] + s[i:]
+    return s
+
+string = 'a1bcd efg!h'
+print(palin(string))
